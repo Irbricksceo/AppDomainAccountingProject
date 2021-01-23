e8c2d378dbb7e056227e76c23d51b051af941c49
Ross Murphy <Irbricksceo@gmail.com>
2021-01-22 23:22:09 -0500

    Undid Last Change

--- profile.php
+++ profile.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['loggedin'])) {
 }
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_PASS = 'password';
 $DATABASE_NAME = 'accountingprojectlogin';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
