4a8e57741482fe95438dc1ad4fd2cc2a67678b00
BuildTools <barrettrose1@gmail.com>
2021-01-27 18:29:26 -0500

    Changed password requirements. Added logic for email activation.

--- register.php
+++ register.php
@@ -31,13 +31,14 @@ if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 
 // Check for invalid characters in username
 // **Unlikely to be used as email address will server as login ID
+// Might be useful for something else
 if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
     exit('Username is not valid!');
 }
 
 // Check for correct number of characters in password
-if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
-	exit('Password must be between 5 and 20 characters long!');
+if (strlen($_POST['password']) > 30 || strlen($_POST['password']) < 8) {
+	exit('Password must be between 8 and 30 characters long!');
 }
 
 // We need to check if the account with that username exists.
@@ -52,15 +53,29 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 		echo 'Username exists, please choose another!';
 	} else {
         // Username doesnt exists, insert new account
-        //**WILL NEED TO ADD LOGIC HERE FOR SENDING ACTIVATION EMAIL TO ADMIN**
-        if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, userrole) VALUES (?, ?, ?, ?)')) {
+        if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, userrole, Active) VALUES (?, ?, ?, ?, ?)')) {
 	        // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
             $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
             //Userrole set to 3(user) on default for registration
             $userrole = 3;
-	        $stmt->bind_param('sssi', $_POST['username'], $password, $_POST['email'], $userrole);
-	        $stmt->execute();
-            echo 'You have successfully registered, you can now login!';
+            //Active set to 0(inactive) on default for registration
+            $Active = 0;
+	        $stmt->bind_param('sssii', $_POST['username'], $password, $_POST['email'], $userrole, $Active);
+            $stmt->execute();
+            
+            //Logic for sending account activation email to admin.
+            $from    = 'noreply@accountingpro.com';
+            $subject = 'New User Account Activation';
+            $headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+            // Update the activation variable below
+            //**WILL REQUIRE CHANGES TO URL UPON PUSH TO LIVE**
+            $activate_link = 'http://localhost/AccountingProject/activate.php?email=' . $_POST['email'];
+            $message = '<p>Please click the following link to activate the account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
+            $adminEmail = 'barrettrose1@gmail.com';
+            mail($adminEmail, $subject, $message, $headers);
+
+
+            echo 'You have successfully registered!  Please wait for account activation before logging in.';
         } else {
 	        // Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
 	        echo 'Could not prepare statement!';
