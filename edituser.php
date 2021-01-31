74906b24c353bc7ebcc5dfe71a49cd9664de4546
Ross Murphy <Irbricksceo@gmail.com>
2021-01-30 19:45:51 -0500

    Ability To Change Emails via edituser form

--- edituser.php
+++ edituser.php
@@ -27,8 +27,34 @@ $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'accountingprojectlogin';
+//Connect to the DB
+$link = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+//Primes the query to pull current data
+$sql = "SELECT * FROM accounts WHERE id='$editu'";
 
+//Gets current user data
+$qry = mysqli_query($link, $sql);
+$data = mysqli_fetch_array($qry);
 
+//Fires update query when form is submitted
+if(isset($_POST['update'])) {
+	$newEmail = $_POST['email'];
+	$sqlupd = "UPDATE accounts SET Email = '$newEmail' WHERE id='$editu'";
+	$edit = mysqli_query($link, $sqlupd);
+	if($edit)
+    {
+        header("location:edituser.php?r=$return&u=$editu"); // reload page
+        exit;
+    }
+    else
+    {
+        echo mysqli_error();
+	}   
+
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -57,8 +83,15 @@ $DATABASE_NAME = 'accountingprojectlogin';
 			<h2>Editing User</h2>
 			<div>
 			   <?php
-			   echo "<h3> Editing User " . $editu . "</h3>"
+			   echo "<h3> Editing User " . $data['username'] . "</h3>"
 			   ?>
+			</div>	
+			<div>
+				<form action="" method="post">
+					<input type="text" name="email" placeholder="Email" value="<?php echo $data['Email'];?> ">
+
+					<input type="submit" value="Update" name="update" >
+				</form>
 			</div>
 		</div>
 	</body>
