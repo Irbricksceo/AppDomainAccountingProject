7c42cb8aad1b76dc52cd44212e74adfad6f572fc
Ross <Irbricksceo@gmail.com>
2021-03-23 17:56:12 -0400

    Script work

--- scripts/batchscripts.php
+++ scripts/batchscripts.php
@@ -6,17 +6,23 @@ function processBatch ($link, $bid, $status) {
     -Update Balances of relavent accounts
     */
     //update status
-	$sqlupd = "UPDATE transactions SET status = $status WHERE batchID = $bid";
+    //TODO: ADD Date assesed and approver updater to line below
+    $aid = $SESSION_['id'];
+	$sqlupd = "UPDATE transactions SET status = $status, approverID = $aid, dateassessed = DATE_ADD(NOW() WHERE batchID = $bid";
      if ($edit = mysqli_query($link, $sqlupd)) {
         if ($status == 1) {
             $sqlupd2 = "SELECT * FROM transactions WHERE batchID = $bid";
             $result = mysqli_query($link, $sqlupd2);
             while($row = mysqli_fetch_array($result)){
+                $acct = //make this the first digit of the account
                 $rowcredit = $row['credit'];
                 $rowdebit = $row['debit'];                               
                 $rowid = $row['accountID'];
-                echo "entering loop for $rowid";
-                $sqlupd3 = "UPDATE faccount SET fbalance = fbalance + $rowcredit - $rowdebit, debit = debit + $rowdebit, credit = credit + $rowcredit WHERE faccountID = $rowid";
+                if ($acct == 0)  {//If its a debit acccount
+                    $sqlupd3 = "UPDATE faccount SET fbalance = fbalance + $rowcredit - $rowdebit, debit = debit + $rowdebit, credit = credit + $rowcredit WHERE faccountID = $rowid";
+                } else {
+                    $sqlupd3 = "UPDATE faccount SET fbalance = fbalance + $rowcredit - $rowdebit, debit = debit + $rowdebit, credit = credit + $rowcredit WHERE faccountID = $rowid";
+                }
                 if ($result2 = mysqli_query($link, $sqlupd3)) {
                     echo "Update Made";
                 } else {
