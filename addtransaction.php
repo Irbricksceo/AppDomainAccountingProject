ced674745be5a79a70dc3932d42cf3ff2fb9badc
Your Name <matthew.j.scheer@gmail.com>
2021-03-23 17:42:30 -0400

    Class update

--- addtransaction.php
+++ addtransaction.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['loggedin'])) {
 	exit;
 }
 
-include 'scripts/userscripts.php'; 										//WHAT IS THIS USED FOR
+//include 'scripts/userscripts.php'; 										//WHAT IS THIS USED FOR
 
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
@@ -18,72 +18,21 @@ $link = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE
 if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-/*
-if ($_SESSION['userrole'] != 1) {
-    header("location:home.php"); // Kick Non Admins backl to home
-    exit;
-}
-*/
+
 //Fires update query when form is submitted
 if(isset($_POST['Create'])) {
     //will actually run the sql
-	
-	/*  OG CODE
-    $AcctName = $_POST['Name'];
-    $Category = $_POST['Category'];
-    $subCategory = $_POST['Subcategory'];
-    $Comment = $_POST['Comment'];
-    $Desc = $_POST['Description'];
-    $startBal = $_POST['StartingBalance'];
-    $createdBy = $_SESSION['id'];
-	*/ 
 	$line = $_POST['lineID'];					//not null auto increment unique
 	$transactionID = $_POST['transactionID'];	//not null
-	$batch = $_POST['batchID'];
+	//$batch = $_POST['batchID'];
 	$account = $_POST['accountID'];		//not null
 	$submitter = $_POST['submitterID'];	
-	$approver = $_POST['approverID'];
-	$datecreated = $_POST['datecreated'];	//current timestamp, date
-	$dateassessed = $_POST['dateassessed']; //current timestamp, date
 	$debit = $_POST['debit'];	//double
 	$credit = $_POST['credit'];	//double
-	$sourcedoc = $_POST['sourceDocument'];	//longblob
 	$status = $_POST['status'];
 
-
-	//not doing catagory things
-
-	/* OG CODE
-    //find number of rows in current category, increments, and *10 it to get the acct number portion
-    $query = "SELECT * FROM faccount WHERE fcategory = $Category"; 
-    $result = mysqli_query($link, $query);  
-    if ($result) 
-    { 
-        $row = mysqli_num_rows($result);
-        $row ++;
-        $row = $row*10;
-    }
-	
-    //concat category with above number to create account ID number
-    $acctID = $Category . $row;
-    // Sets normal Side based on category
-    if ($Category == 1 || $Category == 5) {
-        $normalSide = 0;
-    } else {
-        $normalSide = 1;
-	}
-		*/
-
-
-	//creates the qry to add an acct
-	
-	/*
-	$sqlupd = "INSERT INTO `faccount` (`faccountID`, `faccount`, `fdescription`, `normalside`, `fcategory`, `fsubcategory`, `finitialbalance`, `debit`, `credit`, `fbalance`, `userID`, `comment`, `active`) 
-    VALUES ('$acctID', '$AcctName', '$Desc', '$normalSide', '$Category', '$subCategory', $startBal, 0.00, 0.00, '$startBal', '$createdBy', '$Comment', '1')";
-	*/
-	$sqlupd = "INSERT INTO `transactions` (`lineID`, `transactionID`, `BatchID`, `AccountID`, `SubmitterID`, `ApproverID, `Datecreated`, `dateassessed`, `debit`, `credit`, `sourceDocument`, `status`) 
-    VALUES ('$lineID', '$transactionID', '$Batch', '$account', '$submitter', '$approver', $datecreated, $dateassessed, $debit, '$credit', '$sourcedoc', '$status')";
-	
+	$sqlupd = "INSERT INTO `transactions` (`lineID`, `transactionID`, `BatchID`, `AccountID`, `SubmitterID`, `debit`, `credit`, `status`) 
+    VALUES ('$lineID', '$transactionID', '$Batch', '$account', '$submitter', $debit, '$credit', '3')";
 
     $edit = mysqli_query($link, $sqlupd); //runs the qry
     if($edit) //entered if acct created successfully
@@ -96,6 +45,7 @@ if(isset($_POST['Create'])) {
     {
         echo "Could Not Add Account, SQL Returned Error";
     }
+	//generate batch ID && transaction ID && submitter ID**********************************************************
 }
  
 ?>
@@ -150,27 +100,18 @@ if(isset($_POST['Create'])) {
 		</nav>
 		<div class="content">
 			<h2>Create New Transaction</h2>	
-			<!--
 			<div class="tooltip">Hover For Help
   				<span class="tooltiptext">Make Sure All Fields Are Filled And Press Submit To Add Accounts.</span>
-			-->
 			</div>
 			<div>
 				<h3> Transaction Information </h3>
 				<form action="" method="post">
                     
-				<input type="text" name="lineID" placeholder="lineID" ><br>
 				<input type="text" name="transactionID" placeholder="transactionID" ><br>
-				<input type="text" name="BatchID" placeholder="BatchID" ><br>
 				<input type="text" name="AccountID" placeholder="AccountID" ><br>
-				<input type="text" name="SubmitterID" placeholder="SubmitterID" ><br>
 				<input type="text" name="ApproverID" placeholder="ApproverID" ><br>
-				<input type="text" name="Datecreated" placeholder="Datecreated" ><br>
-				<input type="text" name="dateassessed" placeholder="dateassessed" ><br>
 				<input type="number" name="credit" placeholder="credit" ><br>
 				<input type="number" name="debit" placeholder="debit" ><br>
-				<input type="text" name="sourceDocument" placeholder="sourceDocument" ><br>
-				<input type="text" name="status" placeholder="status" ><br>
 
 					<!--
 					<input type="text" name="Name" placeholder="Account Name" ><br>
