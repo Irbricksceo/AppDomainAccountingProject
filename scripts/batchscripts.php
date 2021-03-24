3369cb0ae404cd9d45c767074ad1f5f9943c8895
Ross Murphy <Irbricksceo@gmail.com>
2021-03-24 18:56:01 -0400

    Updated Batch Processing Script

--- scripts/batchscripts.php
+++ scripts/batchscripts.php
@@ -7,31 +7,32 @@ function processBatch ($link, $bid, $status) {
     */
     //update status
     //TODO: ADD Date assesed and approver updater to line below
-    $aid = $SESSION_['id'];
-	$sqlupd = "UPDATE transactions SET status = $status, approverID = $aid, dateassessed = DATE_ADD(NOW() WHERE batchID = $bid";
+    $aid = $_SESSION['id'];
+    $currentDate = date("Y-m-d");
+	$sqlupd = "UPDATE transactions SET status = $status, approverID = $aid, dateassessed = $currentDate WHERE batchID = $bid";
      if ($edit = mysqli_query($link, $sqlupd)) {
         if ($status == 1) {
             $sqlupd2 = "SELECT * FROM transactions WHERE batchID = $bid";
             $result = mysqli_query($link, $sqlupd2);
             while($row = mysqli_fetch_array($result)){
-                $acct = //make this the first digit of the account
                 $rowcredit = $row['credit'];
                 $rowdebit = $row['debit'];                               
                 $rowid = $row['accountID'];
-                if ($acct == 0)  {//If its a debit acccount
-                    $sqlupd3 = "UPDATE faccount SET fbalance = fbalance + $rowcredit - $rowdebit, debit = debit + $rowdebit, credit = credit + $rowcredit WHERE faccountID = $rowid";
+                $acct = substr($rowid, 0, 1);
+                if ($acct == 1 || $acct == 5)  {
+                    $sqlupd3 = "UPDATE faccount SET fbalance = fbalance - $rowcredit + $rowdebit, debit = debit + $rowdebit, credit = credit + $rowcredit WHERE faccountID = $rowid";
                 } else {
                     $sqlupd3 = "UPDATE faccount SET fbalance = fbalance + $rowcredit - $rowdebit, debit = debit + $rowdebit, credit = credit + $rowcredit WHERE faccountID = $rowid";
                 }
                 if ($result2 = mysqli_query($link, $sqlupd3)) {
                     echo "Update Made";
                 } else {
-                    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+                    exit('Failed to connect to MySQL 1: ' . mysqli_connect_error());
                 }
             }
         }
      } else {
-        exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+        exit('Failed to connect to MySQL 2: ' . mysqli_connect_error());
      }
 }
 ?>
