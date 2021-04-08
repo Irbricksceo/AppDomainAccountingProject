31e02a0c02ff82160acae56fbce7b6517636985d
Ross Murphy <Irbricksceo@gmail.com>
2021-04-08 12:47:02 -0400

    Added generate reports page. Link on home for mngr

--- generatereports.php
+++ generatereports.php
@@ -1,10 +1,15 @@
 <?php
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
-//If the user is not logged in redirect to the login page...
+// If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
+}
+
+if ($_SESSION['userrole'] != 2) {
+    header("location:home.php"); // Kick Non managers back to home
+    exit;
 }
 
 $DATABASE_HOST = 'localhost';
@@ -16,18 +21,43 @@ $link = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE
 if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+
+if(isset($_POST['Generate'])) {
+    $startDate = $_POST['startdate'];
+    $endDate = $_POST['enddate'];
+    $reportType = $_POST['reporttype'];
+
+    //temp echo to verify form function.
+    echo "Generating Report type " . $reportType . " Starting on " . $startDate . " and Going to " . $endDate;
+    //place logic for actually generating reports (or calls to functions/pages that do) here
+    }
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Home Page</title>
+		<title>Generate Reports</title>
 		<link href="css/style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
         <link rel="icon" href="images/favicon.ico">
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+		<style type="text/css">
+        .wrapper{
+            width: 650px;
+            margin: 0 auto;
+        }
+        .page-header h2{
+            margin-top: 0;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    	</style>
 	</head>
 	<body class="loggedin">
-		<nav class="navtop">
+    <nav class="navtop">
 			<div>
 				<img src="images/logo.png" width="60" alt="Logo">
 				<h1>Accounting Pro</h1>
@@ -54,9 +84,9 @@ if (mysqli_connect_errno()) {
 					<a href="approvebatch.php"><i class="fas fa-user-circle"></i>Review Batch</a>
 					<a href="entries.php"><i class="fas fa-user-circle"></i>Journal</a>	
 					<hr><?php
-				}	
+				}
 				?>
-			<h2>Account Management</h2>	 			
+			<h2>Account Management</h2>	
 			<a href="accounts.php"><i class="fas fa-user-circle"></i>Accounts</a>
 			<?php
 				if ($_SESSION['userrole'] == '1'):
@@ -64,42 +94,23 @@ if (mysqli_connect_errno()) {
 					<?php 
 					endif;	
 				?>
-			<a href="eventlog.php"><i class="fas fa-user-circle"></i>Event Log</a>
-			<hr>
-			<h2>Reporting</h2>
-			<?php 
-				if ($_SESSION['userrole'] == '2') { 
-					?><a href="generatereports.php"><i class="fas fa-user-circle"></i>Generate Reports</a>
-					<?php 
-				}
-				?>
 			</div>
 		</nav>
 		<div class="content">
-			<h2>Home Page</h2> 
-			<div class="tooltip">Hover For Help
-  				<span class="tooltiptext">Use the navigation bars to access features.</span>
-			</div>
-			<p>Welcome back, <?=$_SESSION['name']?>!</p>
-			
-			<div>
-			<p>Welcome to Accounting Pro, your online tool for all your accounting needs.</p>
-			<?php 
-			if ($_SESSION['userrole'] == '1'):
-				?><h3> As an administrator, you currently have access to tools for managing users on the platform. Try it out in the users tab!</h3><?php 
-			elseif ($_SESSION['userrole'] == '2'):
-				$sqlSelect="SELECT DISTINCT BatchID FROM transactions WHERE status = 0 ";
-                $result = mysqli_query($link, $sqlSelect);
-                echo "<h3> Notice: There are " . mysqli_num_rows($result) . " batches awaiting approval </h3>"; 
-			elseif ($_SESSION['userrole'] == '3'):
-				$sqlSelect="SELECT DISTINCT BatchID FROM transactions WHERE status = 0 ";
-                $result = mysqli_query($link, $sqlSelect);
-                echo "<h3> Notice: There are " . mysqli_num_rows($result) . " batches awaiting approval </h3>"; 
-			else:
-				?><h3> Unable to retrieve role, please logout and login again. If this problem persists, please contact your administrator.</h3><?php 
-			endif;
-			?>
-			</div>
+			<h2>Generate Reports</h2>
+ 			<div>
+             <form action="" method="post">
+                <label> Report Type </label><select name="reporttype" id="reporttype">
+                        <option value="1">Trial Balance</option>
+                        <option value="2">Income Statement</option>
+                        <option value="3">Balance Sheet</option>
+                        <option value="4">Retained Earnings Statement</option>
+                </select> <br>
+				<label> Start Date </label> <input type="date" name="startdate"><br>
+                <label> End Date </label><input type="date" name="enddate"><br>
+				<input type="submit" value="Generate" name="Generate" >
+			</form>   
+            </div>
 		</div>
 	</body>
 </html>
