914d9e3ade6d2aa2b48ad7a7f0fe0cfead6baf10
BuildTools <barrettrose1@gmail.com>
2021-04-14 23:24:48 -0400

    Implemented income statement w/ DataTables

--- reportsFetchData.php
+++ reportsFetchData.php
@@ -14,10 +14,9 @@ $reportType = $_GET['reportID'];
 $startDate = $_GET['startDate'];
 $endDate = $_GET['endDate'];
 
-
-//For testing SQL queries
 /*
-$reportType = 1;
+//For testing SQL queries
+$reportType = 2;
 $startDate = "2021-01-01";
 $endDate = "2021-04-14";
 */
@@ -108,7 +107,7 @@ switch ($reportType) {
 
         while($row = mysqli_fetch_array($result)){
             
-            //Need to declare outside for loop for if statement to make new row with new accountID 
+            //Need to declare outside for loop for if statement that makes new row with new accountID 
             $i = 0;
 
             //Search through data['accountID'] for existing accountID
@@ -248,7 +247,7 @@ switch ($reportType) {
 
         //Place formatted totals into new row and add to data[]
             //Assigning accountID of 999 and hiding it to force total row to be last row of report
-        $newRow['accountID'] = '<b style="display:none">' . 999 . '</b>';
+        $newRow['accountID'] = '<b style="display:none">' . 999.9 . '</b>';
         $newRow['faccount'] = "";
         $newRow['debit'] = '<b>' . number_format($totalDebit, 2) . '</b>';
         $newRow['credit'] = '<b>' . number_format($totalCredit, 2) .  '</b>';
@@ -260,22 +259,174 @@ switch ($reportType) {
     //Income Statement
     case 2:
 
-        $sql = "";
+        $sql = "SELECT t.accountID, t.debit, t.credit, fa.faccount, fa.normalside FROM transactions t 
+        JOIN faccount fa ON t.accountID = fa.faccountID WHERE t.dateassessed BETWEEN '$startDate' AND '$endDate' 
+        AND t.status = 1 AND fa.fcategory = 4 OR fa.fcategory = 5";
+
         $result = mysqli_query($link, $sql);
 
+        $totalRevenue = 0;
+        $totalExpense = 0;
+
         while($row = mysqli_fetch_array($result)){
-            //Write conversions here
             
-            //Add row into data array
-            $data[] = $row;
+            //Need to declare outside for loop for if statement that makes new row with new accountID 
+            $i = 0;
+
+            //Search through data['accountID'] for existing accountID
+            for ($i = 0; $i < count($data); $i++) 
+            {
+                //AccountID exists in data[] and matches accountID of current row
+                if ($row['accountID'] == $data[$i]['accountID'])
+                {
+                    //Add debit/credit of transaction to debit/credit column of the row with a matching accountID in data[]
+                    //Expense(debit) account
+                    if($data[$i]['normalside'] == 0)
+                    {
+                        //Expense(debit) account is being debited (amount is increasing)
+                        if($row['debit'] > 0)
+                        {
+                            $data[$i]['balance'] += $row['debit'];
+                            $totalExpense += $row['debit'];
+                        }
+                        //Expense(debit) account is being credited (amount is decreasing)
+                        if($row['credit'] > 0)
+                        {
+                            $data[$i]['balance'] -= $row['credit'];
+                            $totalExpense -= $row['credit'];
+                        }
+                    }
+                    //Revenue(credit) account
+                    if($data[$i]['normalside'] == 1)
+                    {
+                        //Revenue(credit) account is being credited (amount is increasing)
+                        if($row['credit'] > 0)
+                        {
+                            $data[$i]['balance'] += $row['credit'];
+                            $totalRevenue += $row['credit'];
+                        }
+                        //Revenue(credit) account is being debited (amount is decreasing)
+                        if($row['debit'] > 0)
+                        {
+                            $data[$i]['balance'] -= $row['debit'];
+                            $totalRevenue -= $row['debit'];
+                        }
+                    }
+
+                    //Exit for loop
+                    break;
+                }
+            }//End for loop
+            
+            //For loop maxed out, could not find existing accountID in data[], need to create new row with new accountID 
+            if ($i == count($data))
+            {
+                //Create new row with accountID
+                $newRow['accountID'] = $row['accountID'];
+                $newRow['faccount'] = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $row['faccount'];
+                $newRow['normalside'] = $row['normalside'];
+                
+                //Add debit/credit of transaction to debit/credit of accountID in data[]
+                //Expense(debit) account
+                if($row['normalside'] == 0)
+                {
+                    //Expense(debit) account is being debited (amount is increasing)
+                    if($row['debit'] > 0)
+                    {
+                        $newRow['balance'] = $row['debit'];
+                        $totalExpense += $row['debit'];
+                    }
+                    //Expense(debit) account is being credited (amount is decreasing)
+                    if($row['credit'] > 0)
+                    {
+                        $newRow['balance'] = (-1 * $row['credit']);
+                        $totalExpense -= $row['credit'];
+                    }
+                }
+                //Revenue(credit) account
+                if($row['normalside'] == 1)
+                {
+                    //Revenue(credit) account is being credited (amount is increasing)
+                    if($row['credit'] > 0)
+                    {
+                        $newRow['balance'] = $row['credit'];
+                        $totalRevenue += $row['credit'];
+                    }
+                    //Revenue(credit) account is being debited (amount is decreasing)
+                    if($row['debit'] > 0)
+                    {
+                        $newRow['balance'] = (-1 * $row['debit']);
+                        $totalRevenue -= $row['debit'];
+                    }
+                }
+
+                //Add new row into data
+                $data[] = $newRow;
+            }
+
+        }//End while loop to recieve rows from SQL query
+
+        //Add final formatting and parentheses (if necessary) for balances 
+        for($i = 0; $i < count($data); $i++)
+        {
+            //Check if balance value is negative
+            if ($data[$i]['balance'] < 0)
+            {
+                //Apply formatting and parentheses
+                $data[$i]['balance'] = "(" . number_format(abs($data[$i]['balance']), 2) . ")";
+            }
+            //Balance value is positive
+            else
+            {
+                //Apply formatting
+                $data[$i]['balance'] = number_format($data[$i]['balance'], 2);
+            }
         }
+        /*
+        //Place formatted totals into new row and add to data[]
+            //Assigning accountID of 999 and hiding it to force total row to be last row of report
+        $newRow['accountID'] = '<b style="display:none">' . 999 . '</b>';
+        $newRow['faccount'] = "";
+        $newRow['debit'] = '<b>' . number_format($totalDebit, 2) . '</b>';
+        $newRow['credit'] = '<b>' . number_format($totalCredit, 2) .  '</b>';
+        $data[] = $newRow;
+        */
 
-        //Create new row for aggregated data/totals
+        //Format and add header/footer rows in data[] to match report format
+        $headerRow['accountID'] = '<b style="display:none">' . 399.9 . '</b>';
+        $headerRow['faccount'] = '<b>' . "Revenues" . '</b>';
+        $headerRow['balance'] = "";
+        $data[] = $headerRow;
+        $headerRow['accountID'] = '<b style="display:none">' . 499.8 . '</b>';
+        $headerRow['faccount'] = '<b>' . "Total Revenues" . '</b>';
+        $headerRow['balance'] = '<b>' . number_format($totalRevenue, 2) . '</b>';
+        $data[] = $headerRow;
+        $headerRow['accountID'] = '<b style="display:none">' . 499.9 . '</b>';
+        $headerRow['faccount'] = '<b>' . "Expenses" . '</b>';
+        $headerRow['balance'] = "";
+        $data[] = $headerRow;
+        $headerRow['accountID'] = '<b style="display:none">' . 599.9 . '</b>';
+        $headerRow['faccount'] = '<b>' . "Total Expenses" . '</b>';
+        $headerRow['balance'] = '<b>' . number_format($totalExpense, 2) . '</b>';
+        $data[] = $headerRow;
 
+        //Add net income row
+        $netIncome = $totalRevenue - $totalExpense;
+        $headerRow['accountID'] = '<b style="display:none">' . 999 . '</b>';
+        $headerRow['faccount'] = '<b>' . "Net Income (Loss)" . '</b>';
+        //Check if netIncome is negative
+        if($netIncome < 0)
+        {
+            $headerRow['balance'] = '<b>' . "(" . number_format(abs($netIncome), 2) . ")" . '</b>';
+        }
+        //netIncome is positive
+        else
+        {
+            $headerRow['balance'] = '<b>' . number_format($netIncome, 2) . '</b>';
+        }
+        $data[] = $headerRow;
 
-        //Add new row to data
-
-
+        //Exit switch statement
         break;
 
     //Balance Sheet
